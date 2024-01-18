6a5b43cdd6e1eb004cc4b39b173a6b6a37f43a54
busra <busraaceval@gmail.com>
2024-01-19 00:12:43 +0300

    ADD: dashboard graph

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import { useEffect } from "react";
+import Charts from "../components/Charts";
+import KpiCards from "../components/KpiCards";
 import useStockCalls from "../hooks/useStockCalls";
 
 const Home = () => {
@@ -16,6 +18,8 @@ const Home = () => {
       <Typography variant="h4" color="error" mb={4}>
         Dashboard
       </Typography>
+      <KpiCards />
+      <Charts />
     </Box>
   );
 };
